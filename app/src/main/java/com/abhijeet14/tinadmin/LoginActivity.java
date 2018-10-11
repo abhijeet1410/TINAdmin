be90b0744a1606322a54b2378d3ec503abaee2ce
abhijeet1410 <42213151+abhijeet1410@users.noreply.github.com>
2018-10-11 15:05:07 +0530

    Modified

---
+++ app/src/main/java/com/abhijeet14/tinadmin/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.abhijeet14.tinadmin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
